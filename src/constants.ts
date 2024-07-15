78bf0f2c2edd586c9a9fa7192a5c0f55662c2f69
april <hesiyuetian@outlook.com>
2024-07-15 10:56:21 +0800

    update

--- src/constants.ts
+++ src/constants.ts
@@ -23,14 +23,14 @@ export type ARWEAVE_TYPE = {
     PROVIDER_URL: string;
     URL: string;
     TOKEN: string;
-}
+};
 
 export type APP_ENV_TYPE = {
     PORT: string;
     MONGODB: APP_ENV_MONGODB_TYPE;
-    BNB_GREENFIELD: BNB_GREENFIELD_TYPE
-    ARWEAVE: ARWEAVE_TYPE
-}
+    BNB_GREENFIELD: BNB_GREENFIELD_TYPE;
+    ARWEAVE: ARWEAVE_TYPE;
+};
 
 export const APP_ENV: APP_ENV_TYPE = {
     PORT: process.env.PORT + '',
@@ -40,21 +40,21 @@ export const APP_ENV: APP_ENV_TYPE = {
         DB: process.env.MONGODB_DB + '',
         USER: process.env.MONGODB_USER + '',
         PASSWORD: process.env.MONGODB_PASSWORD + '',
-        SSLCA: process.env?.MONGODB_SSLCA
+        SSLCA: process.env?.MONGODB_SSLCA,
     },
     BNB_GREENFIELD: {
         HOST: process.env.BNBGREENFIEL_HOST + '',
         PORT: process.env.BNBGREENFIEL_PORT + '',
         ENDPOINTS: process.env.BNBGREENFIEL_ENDPOINT + '',
         ACCOUNT: process.env.BNBGREENFIELD_ACCOUNT_ADDRESS + '',
-        PRIVATE_KEY: process.env.BNBGREENFIELD_ACCOUNT_PRIVATEKEY + ''
+        PRIVATE_KEY: process.env.BNBGREENFIELD_ACCOUNT_PRIVATEKEY + '',
     },
     ARWEAVE: {
         URL: process.env.ARWEAVE_URL + '',
         TOKEN: process.env.ARWEAVE_TOKEN + '',
         PRIVATE_KEY: process.env.ARWEAVE_PRIVATE_KEY + '',
         PROVIDER_URL: process.env.ARWEAVE_PROVIDER_URL + '',
-    }
+    },
 };
 
 export const AES_SECRET_KEY: string = process.env.AES_SECRET_KEY + '';
@@ -67,3 +67,10 @@ export enum MIDJOURNEY_MODE {
     FAST,
     RELAX,
 }
+
+export const SOLANA_ENV = {
+    SOLANA_NET: process.env.SOLANA_NET + '',
+    SOLANA_RPC_URL: process.env.SOLANA_RPC_URL + '',
+    SOLANA_PALTFORM_ACCOUNT: process.env.SOLANA_PALTFORM_ACCOUNT + '',
+    SOLANA_FEEPAYER_PRIVATE_KEY: process.env.SOLANA_FEEPAYER_PRIVATE_KEY + '',
+};
--- src/routers/transaction.ts
+++ src/routers/transaction.ts
@@ -1,11 +1,64 @@
 import { Router } from 'express';
 import Result from '../utils/result';
+import { Keypair, PublicKey, Transaction } from '@solana/web3.js';
+import * as bs58 from 'bs58';
+import { SOLANA_ENV } from '../constants';
+import { USDC_DEV_PUBKEY, USDC_PUBKEY } from '../config/pubkeys.config';
+import { getAssociatedTokenAddress } from '@solana/spl-token';
+import { SolTransaction, TokenInformation } from '../helpers/solana';
+import BigNumber from 'bignumber.js';
 const router: Router = Router();
+const solTransaction: SolTransaction = new SolTransaction();
 
 router.get('/send', async (req: any, res) => {
     try {
         const { amount, id, debug } = req.query;
         res.send({ amount, id, debug });
+    } catch (e: any) {
+        res.send(Result.err(500, e.message || String(e)));
+    }
+});
+router.get('/pay', async (req: any, res) => {
+    try {
+        let { amount, id, debug, payCurrency } = req.query;
+        payCurrency = payCurrency || 'SOL';
+
+        // Build the payment transaction
+        const sender = new PublicKey(SOLANA_ENV.SOLANA_PALTFORM_ACCOUNT);
+        const receiver = new PublicKey(SOLANA_ENV.SOLANA_PALTFORM_ACCOUNT);
+        const quantity = Number(amount);
+        const feePayer = Keypair.fromSecretKey(bs58.decode(SOLANA_ENV.SOLANA_FEEPAYER_PRIVATE_KEY));
+
+        let transaction: Transaction;
+        if (payCurrency === 'USDC') {
+            // Pay by USDC
+            let usdcPubkey = USDC_DEV_PUBKEY;
+            if (SOLANA_ENV.SOLANA_NET == 'MAINNET') {
+                usdcPubkey = USDC_PUBKEY;
+            }
+            const receiverAta = await getAssociatedTokenAddress(usdcPubkey, receiver);
+            const token = new TokenInformation('USDC', usdcPubkey, 6);
+            transaction = await solTransaction.buildTransferTokenTransaction(sender, receiver, receiverAta, token, BigInt(new BigNumber(quantity).shiftedBy(6).toFixed()), true, feePayer.publicKey);
+        } else {
+            // Pay by SOL
+            transaction = await solTransaction.buildTransferSolTransaction(sender, receiver, BigInt(new BigNumber(quantity).shiftedBy(9).toFixed()), feePayer.publicKey);
+        }
+
+        // Serialize and deserialize the transaction. This ensures consistent ordering of the account keys for signing.
+        transaction = Transaction.from(
+            transaction.serialize({
+                verifySignatures: false,
+                requireAllSignatures: false,
+            })
+        );
+
+        // Generate signature
+        transaction.partialSign(feePayer);
+
+        // Serialize and return
+        const base = transaction.serialize({ requireAllSignatures: false, verifySignatures: false }).toString('base64');
+        // res.send({ amount, id, debug });
+        return { transaction: base, message: 'Tranasction created successfully' };
     } catch (e: any) {
         res.send(Result.err(500, e.message || String(e)));
     }
